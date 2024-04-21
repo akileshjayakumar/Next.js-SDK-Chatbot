e9400d09aa2e9a950cf7a88a5e9dec49a292dde1
Akilesh <ja694@uowmail.edu.au>
2024-04-21 14:27:46 +0800

    Refactor page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import Chat from "@/components/chat";
-import Head from "next/head";
 
 export const runtime = "edge";
 
 export default function Page() {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="src/app/favicon.ico" />
-      </Head>
-      <Chat />
-    </>
-  );
+  return <Chat />;
 }
