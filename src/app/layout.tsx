ab527b4e1e88540747eb692c6466a306043668dc
Akilesh Jayakumar <ja694@uowmail.edu.au>
2024-04-26 20:10:17 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next.js SDK ChatBot",
+  title: "Next.js14 SDK ChatBot",
 };
 
 export default function Layout({
