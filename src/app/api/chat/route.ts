7bd401e55fc36e8141297013a2c5d050d01d194d
Akilesh Jayakumar <ja694@uowmail.edu.au>
2024-04-27 14:06:52 +0800

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
 
   // Request the OpenAI API for the response based on the prompt
   const response = await openai.chat.completions.create({
-    model: "gpt-4",
+    model: "gpt-3.5-turbo-0125",
     stream: true,
     messages: messages,
   });
